2682c253011d1c215959b4ab8a6a4fbf4226be02
Kshu27 <106130735+keshavmishra27@users.noreply.github.com>
2026-02-13 22:52:37 +0530

    custom api creation
    created custom api of logic in software

---
+++ custom_api.py
@@ -0,0 +1,15 @@
+
+from fastapi import FastAPI
+from logic import process_frame
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Face Brightness Controller Running"}
+
+@app.get("/check-faces")
+def check_faces():
+    result = process_frame()
+    return resultfrom face_brightness 
+
